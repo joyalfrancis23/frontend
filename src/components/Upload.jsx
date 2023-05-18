c23c99f94b7dc93b2fe19ef663472bad06c1e57e
joyalfrancis23 <joyalfrancis23@gmail.com>
2023-05-18 19:53:55 +0530

    backend added

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -21,8 +21,23 @@ const Upload = () => {
     setForm({ ...form, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    try{
+      const response = await fetch('http://127.0.0.1:8000/submit/',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      console.log("Data SendED!");
+    }catch(error){
+      console.log(error.body);
+      console.log("Somethings not right");
+    }
+
     setLoading(true);
   };
 
